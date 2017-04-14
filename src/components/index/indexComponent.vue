aa4fac22cd0a32bcac3db2ec1e9ceefd459b50a2
Xqrrrrrr <952066976@qq.com>
2017-04-14 15:50:56 +0800

    begin

---
+++ src/components/index/indexComponent.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>这是主页</h1>
+</template>
+
+<script type="text/javascript">
+
+	export default {
+		name: 'index',
+		data: function(){
+			return {}
+		}
+	}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import http from '../utils/HttpClient'
+
+import indexComponent from '../components/index/indexComponent.vue'
+import LoginComponent from '../components/login/LoginComponent.vue'
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	routes: [{
+		path: '/',
+		name: 'index',
+		component: indexComponent
+	},{
+		path: '/login',
+		name: 'login',
+		component: LoginComponent
+	}]
+})
+
+//对将要进入的路由进行权判断
+// router.beforeEach((to, from, next) =>{
+// 	if(to.path != '/login'){
+// 		// http.get('./src/data/sesson.json')
+// 		// .then(response => {
+// 		// 	if(!response.login){
+// 		// 		router.replace('login');
+// 		// 	}
+// 		// 	next();
+// 		// })
+// 		// http.get('http://localhost:888/getsession')
+// 		// .then(response => {
+// 		// 	if(!response.name){
+// 		// 		router.replace('login');
+// 		// 	}
+// 		// 	next()
+// 		// });
+// 	} else {
+// 		// next();
+// 	}
+// })
+
+export default router
